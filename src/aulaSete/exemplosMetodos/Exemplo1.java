cb95c96dfa325fb82d7adf8a27879636bc3d8ba1
Rodrigo Henrich <rodrigohenrich@faccat.br>
2024-04-12 16:19:02 -0300

    Exemplos métodos

--- src/aulaSete/exemplosMetodos/Exemplo1.java
+++ src/aulaSete/exemplosMetodos/Exemplo1.java
@@ -1,2 +1,31 @@
-package aulaSete.exemplosMetodos;public class Exemplo1 {
+package aulaSete.exemplosMetodos;
+
+import java.util.Scanner;
+
+public class Exemplo1 {
+
+	public static int soma(int v1, int v2){
+		int resp = v1+v2;
+		return resp;
+	}
+
+	public static int soma(int v1, int v2, int v3){
+		int resp = v1+v2+v3;
+		return resp;
+	}
+
+	public static String soma(String s1, String s2){
+		return s1+" "+s2;
+	}
+
+	public static void main(String[] args) {
+		Scanner ler = new Scanner(System.in);
+		System.out.print("Digite um número: ");
+		int n1 = ler.nextInt();
+		System.out.print("Digite outro número: ");
+		int n2 = ler.nextInt();
+		int resposta = soma(n1,n2);
+		System.out.println("A soma vale "+resposta);
+	}
+
 }
--- src/aulaSete/exemplosMetodos/Exemplo2.java
+++ src/aulaSete/exemplosMetodos/Exemplo2.java
@@ -1,2 +1,31 @@
-package aulaSete.exemplosMetodos;public class Exemplo2 {
+package aulaSete.exemplosMetodos;
+
+import java.util.Random;
+
+public class Exemplo2 {
+
+	static int somaVetor(int[] vetor){
+		int soma=0;
+		for(int i=0;i<vetor.length;i++){
+			soma+=vetor[i];
+		}
+		return soma;
+	}
+
+	static void geraVetor(int[] vetor){
+		Random aleatorio = new Random();
+		for(int i=0;i<vetor.length;i++){
+			vetor[i] = aleatorio.nextInt(20);
+		}
+	}
+	public static void main(String[] args) {
+		int[] lista = {1,4,5,9};
+		int soma = somaVetor(lista);
+		System.out.println(soma);
+		int[] novoVetor = new int[10];
+		geraVetor(novoVetor);
+		for(int i=0;i<novoVetor.length;i++){
+			System.out.print(novoVetor[i]+" ");
+		}
+	}
 }
